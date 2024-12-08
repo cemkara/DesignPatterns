6883ce5473f8f64f07aae61376431d2b65222c92
Cem Kara <cemkara1992@gmail.com>
2024-12-08 09:48:20 +0300

    Add project files.

---
+++ ChainofResponsibility/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChainofResponsibility.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
