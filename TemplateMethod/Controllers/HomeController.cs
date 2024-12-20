7e961467c3e5817ca1d70a7303fe460461efdcd8
Cem Kara <cemkara1992@gmail.com>
2024-12-20 07:53:03 +0000

    Template pattern eklendi

---
+++ TemplateMethod/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TemplateMethod.Models;
+
+namespace TemplateMethod.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult BasicPlan()
+    {
+        Plans plan = new BasicPlan();
+        plan.PlanType("Basic Plan");
+        plan.PersonCount(1)
+        plan.Price(50.00);
+        plan.Resolution("1080p")
+        plan.Content("Basic Plan Content");
+        return View(plan);
+    }
+
+     public IActionResult StandardPlan()
+    {
+        Plans plan = new StandardPlan();
+        plan.PlanType("Standart Plan");
+        plan.PersonCount(2)
+        plan.Price(100.00);
+        plan.Resolution("1080p")
+        plan.Content("Standart Plan Content");
+        return View(plan);
+    }
+    public IActionResult UltraPlan()
+    {
+        Plans plan = new UltraPlan();
+        plan.PlanType("Basic Plan");
+        plan.PersonCount(3)
+        plan.Price(200.00);
+        plan.Resolution("1080p")
+        plan.Content("Ultra Plan Content");
+        return View(plan);
+    }
+
+}
---
+++ TemplateMethod/TemplatePattern/UltraPlan.cs
@@ -0,0 +1,18 @@
+public class UltraPlan:Plans
+{
+    public override string PlanType(string type){
+        return type;
+    }
+    public override int PersonCount(string count){
+        return count;
+    }
+    public override double Price(string price){
+        return price;
+    }
+    public override string Resolution(string resolution){
+        return resolution;
+    }
+    public override string Content(string content){
+        return Content;
+    }
+}
